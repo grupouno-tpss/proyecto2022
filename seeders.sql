df1bcea221c63f09c54a1474413057f42ea0cf37
grupouno-tpss <88520777+grupouno-tpss@users.noreply.github.com>
2022-02-06 14:15:38 -0500

    Update seeders.sql
    Seeder de estado.

--- seeders.sql
+++ seeders.sql
@@ -7,9 +7,22 @@ VALUES
 INSERT INTO
     rol_usuario (idrol_usuario, rol)
 VALUES
-(2, "TRABAJADOR");
+    (2, "TRABAJADOR");
 
 INSERT INTO
     rol_usuario (idrol_usuario, rol)
 VALUES
-(3, "ADMINISTRADOR");
+    (3, "ADMINISTRADOR");
+
+/*Estado reserva*/
+INSERT INTO
+    `estado`(`idestado`, `estado`)
+VALUES
+    (1, "ACTIVO");
+
+INSERT INTO
+    `estado`(`idestado`, `estado`)
+VALUES
+    (2, "ARCHIVADO")
+
+
