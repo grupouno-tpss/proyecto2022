b7d1769cc5ce9b8bc28399327259b477d0c80bc5
Gbandres97 <99147885+Gbandres97@users.noreply.github.com>
2022-02-06 17:12:54 -0500

    Ultima modificacion
    6-2-2022

--- views/admint.php
+++ views/admint.php
@@ -4,7 +4,7 @@ session_start();
 // if (isset($_SESSION["usuario"]) and $_SESSION["contraseña"] and $_SESSION["rol"]) {
 //     echo 'Session iniciada';
 // } else {
-//     echo "No ha iniciado sección";
+//     echo "No ha   iniciado sección";
 //     die();
 // }
 //error_reporting(E_ALL ^ E_NOTICE);
@@ -14,6 +14,7 @@ require_once("../php/usuarioTrabajador.php");
 extract($_REQUEST);
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,68 +25,163 @@ extract($_REQUEST);
     <title>Document</title>
     <script src="../javascript/noResponsive.js"></script>
 
-    <link rel="stylesheet" href="../css/admint.css">
+    <link rel="stylesheet" href="../css/admint.css"> --
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script>
 
-    </script>
 </head>
 
 <body>
     <?php
     // include "pages/loading.php";
     include "pages/nav.php";
-    ?>
-    <!-- <nav>
-        <h1>SRI</h1>
-        <h1>Restaurante Ichiraku Ramen</h1>
-        <h2>Usuario: Jhojann Triana</h2>
-    </nav> -->
-    <div class="content justify-content-around">
-        <div class="d-flex">
-            <div>Empleados</div>
-            <div>Clientes</div>
-            <div>Reservaciones</div>
+    ?> 
+    
+        <br> <br> <br>
+        <div class="content">
+        <a href="#" onclick='req("multimedia/admint.png")'>RF</a>
+        <br>
+        
+        <h1> 
+            <div class="titulos">Seleccione una opción</div>
+        </h1>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#informeModal" id="informeBotton">
+            Realizar informe
+            </button>
+            
+            <!-- Modal -->
+            <div class="modal fade" id="informeModal" tabindex="-1" aria-labelledby="informeModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="informeModalLabel">GENERAR INFORME</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                        <div>
+                            <div class="admint2">
+
+                                <form action="">
+                                    <label for="fecha">Establecer una fecha</label>
+                                    <input type="date" class="form-control">
+                                    <br>
+                                    <label for="hora">Establecer una hora</label>
+                                    <br>
+                                    <input type="time" class="form-control">
+                                    <br>
+    
+                                </form>
+                            </div>
+                        </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Salir</button>
+                                <button type="button" class="btn btn-primary">Generar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </form><br><br><br>
+        </div>
+        <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#empleadosRegistradosModal" id="empleadosRegistradosBotton">
+            Usuarios
+            </button>
+
+        <!-- Modal -->
+            <div class="modal fade" id="empleadosRegistradosModal" tabindex="-1" aria-labelledby="empleadosRegistradosModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <h5 class="modal-title" id="empleadosRegistradosModalLabel">CRUD</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+                </div>
+            </div>
+            </div>        
+            <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#registrousuarioModal" id="registrousuarioBotton">
+                    Registrar usuario
+                </button>
+
+            <!-- Modal -->
+                <div class="modal fade" id="registrousuarioModal" tabindex="-1" aria-labelledby="registrousuarioModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="registrousuarioModalLabel">Registro de usuarios</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                            <div class="modal-body">
+                                <form action="" method="post">
+                                    <label for="">Ingrese su Nombre</label>
+                                    <div class="input-group mb-3">
+                                        <input type="text" class="form-control" placeholder="Primer nombre" aria-label="p_nombre">
+    
+                                        <input type="text" class="form-control" placeholder="Segundo nombre" aria-label="s_nombre">
+    
+                                    </div>
+    
+                                    <label for="">Ingrese su Apellido</label>
+                                    <div class="input-group mb-3">
+    
+                                        <input type="text" class="form-control" placeholder="Primer Apellido" aria-label="p_Apellido">
+    
+                                        <input type="text" class="form-control" placeholder="Segundo Apellido" aria-label="s_Apellido">
+    
+    
+                                    </div>
+                                    <label for="">Ingrese sus Numeros de contacto</label>
+                                    <div class="input-group mb-3">
+    
+                                        <input type="text" class="form-control" placeholder="Numero Celular" aria-label="numeroCelular">
+    
+                                        <input type="text" class="form-control" placeholder="Numero Telefono" aria-label="numeroTelefono">
+    
+                                    </div>
+                                    <label for="">Ingrese su Correo electronico</label>
+                                    <div class="input-group mb-3">
+                                    
+                                        <input type="email" class="form-control" placeholder="Correo electronico" aria-label="email" aria-describedby="basic-addon2">
+                                        <span class="input-group-text" id="basic-addon2">@gmail.com</span>
+                                    </div>
+                                    
+                                    <div class="container">
+                                    
+                                    <div class="conf">Ingrese la contraseña</div>
+                                
+                                    <input type="password" class="form-control" placeholder="Contraseña" aria-label="contraseña">
+                                    </div>
+                                    </div> 
+                            </form>
+                            </div>
+                            </div>
+                            <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Salir</button>
+                            <button type="button" class="btn btn-primary">Guardar Cambios</button>
+                        </div>
+                    </div>
+                    <h1> 
+                        <div class="titulos">Aqui va la tabla</div>
+                    </h1>
+                </div>
+                
+            </div>
+            <br><br><br>
+            
+            <hr><br>
+
+
+
         </div>
     </div>
-    <?php
-    // $addUsT = new usuarioTrabajador();
-    // $addUsT->setPnombre($_REQUEST["Pnombre"]);
-    // $addUsT->setSnombre($_REQUEST["Snombre"]);
-    // $addUsT->setPapellido($_REQUEST["Papellido"]);
-    // $addUsT->setSapellido($_REQUEST["Sapellido"]);
-    // $addUsT->setTipodocument($_REQUEST["tipoDocumento"]);
-    // $addUsT->setNumdocument($_REQUEST["numDocumento"]);
-    // $addUsT->setRol($_REQUEST["rol"]);
-    // $addUsT->setEmail($_REQUEST["correo"]);
-    // $addUsT->setNumeroTelefonico($_REQUEST["numTelefono"]);
-    // $addUsT->setNumeroCelular($_REQUEST["numCelular"]);
-    // $addUsT->setContraseña(md5($_REQUEST["contraseña"]));
-
-
-    // echo $addUsT->getPnombre() . "<br>";
-    // echo $addUsT->getSnombre() . "<br>";
-    // echo $addUsT->getPapellido() . "<br>";
-    // echo $addUsT->getSapellido() . "<br>";
-    // echo $addUsT->getTipodocument() . "<br>";
-    // echo $addUsT->getNumdocument() . "<br>";
-    // echo $addUsT->getRol() . "<br>";
-    // echo $addUsT->getEmail() . "<br>";
-    // echo $addUsT->getNumeroTelefonico() . "<br>";
-    // echo $addUsT->getNumeroCelular() . "<br>";
-    // echo $addUsT->getContraseña();
-    // echo $addUsT->agregarUsuario() . "<br><br>";
-
-
-    ?>
-    <script src="../javascript/wroker.js"></script>
-    <script src="../javascript/postImg.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script>
-        window.onload = function() {
-            document.getElementById("loading").hidden = true;
-        }
-    </script>
 </body>
 
 </html>
